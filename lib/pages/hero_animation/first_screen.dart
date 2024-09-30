dbef90955b8a8cfe363383ab075636fad59bb086
Abhay kumar bhumihar <abhayrai637@gmail.com>
2024-09-30 12:24:52 +0530

    Hero animation updated

--- lib/pages/hero_animation/first_screen.dart
+++ lib/pages/hero_animation/first_screen.dart
@@ -1,6 +1,6 @@
 import 'package:animation_flutter/pages/hero_animation/second_screen.dart';
 import 'package:flutter/material.dart';
-class FirstScreen extends StatelessWidget {
+class HeroAnmiationFirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
